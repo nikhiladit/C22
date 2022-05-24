431504cbc5c974813055f539373ac63808285bde
nikhiladit <93851363+nikhiladit@users.noreply.github.com>
2022-05-24 04:07:39 -0700

    Add files via upload

---
+++ Cannon.js
@@ -0,0 +1,21 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannon=loadImage("./assets/canon.png");
+    this.cannonbase=loadImage("./assets/cannonBase.png");
+  }
+  
+  display() {
+    push()
+    imageMode(CENTER)
+    image(this.cannon,this.x,this.y,this.width,this.height)
+    pop()
+
+    image(this.cannonbase,70,20,200,200)
+    noFill()
+  }
+}
